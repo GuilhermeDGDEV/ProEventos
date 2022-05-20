352451e3d2d1e51d94536c28231a2609e6645499
GuilhermeDGDEV <guilhermediasgoncalvesdev@gmail.com>
2022-05-19 23:13:50 -0300

    Start the backend of application

---
+++ backend/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+
+        public IEnumerable<Evento> _eventos = new Evento[] {
+                new Evento {
+                    EventoId = 1,
+                    Tema = "Criando REST API",
+                    Local = "São Paulo",
+                    Lote = "1º Lote",
+                    QtdPessoas = 250,
+                    DataEvento = DateTime.Now.AddDays(2).ToString(),
+                    ImagemURL = "foto.png"
+                },
+                new Evento {
+                    EventoId = 2,
+                    Tema = "Criando Front End",
+                    Local = "São Paulo",
+                    Lote = "2º Lote",
+                    QtdPessoas = 350,
+                    DataEvento = DateTime.Now.AddDays(3).ToString(),
+                    ImagemURL = "foto.png"
+                }
+            };
+
+        public EventoController() { }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _eventos;
+        }
+
+        [HttpGet("{id}")]
+        public Evento GetById(int id)
+        {
+            return _eventos.Where(evento => evento.EventoId == id).FirstOrDefault();
+        }
+        
+    }
+}
